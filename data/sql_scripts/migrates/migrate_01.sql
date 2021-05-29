5eef02f3fc2391d722c5c6010502f48e60f4f63e
stdmedoth <jovictor210@gmail.com>
2021-05-29 01:38:56 -0300

    modelo base de migrate

--- data/sql_scripts/migrates/migrate_01.sql
+++ data/sql_scripts/migrates/migrate_01.sql
@@ -1,1 +1,4 @@
 -- versao base sem migrate
+
+update versao set versao = '1.1', lancamento = '2021-05-28';
+insert into migrate values(1, 'Versao 1.1', 'migrate_01.sql', '1.2', '2021-05-28');
---
+++ data/sql_scripts/migrates/migrate_03.sql
@@ -0,0 +1,2 @@
+update versao set versao = '1.1', lancamento = '2021-05-29';
+insert into migrate values(3, 'Versao x', 'migrate_03.sql', 'x', '2021-05-29');
