276469994456c6e64596c56b4030d7f1635ff57b
stdmedoth <jovictor210@gmail.com>
2021-07-08 23:18:12 -0300

    correcao ponteiro de arquivo tmp_updt_fp não liberado

--- src/etc/check_updated.c
+++ src/etc/check_updated.c
@@ -1,8 +1,8 @@
 int check_updated(){
 	FILE *tmp_updt_fp = fopen(ATUALIZA_VERTMP,"r");
-    if(!tmp_updt_fp){
-      return 0;
-    }
-
-    return 1;
+  if(!tmp_updt_fp){
+    return 0;
+  }
+  fclose(tmp_updt_fp);
+  return 1;
 }
